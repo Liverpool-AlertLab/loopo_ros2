52f59d2ca564a1526f8091d4cd62d902a2952c38
itsameWolf <lupo961@gmail.com>
2023-07-10 11:43:56 +0100

    Adapted driver to new communication protocol

--- loopo_driver/loopo_driver/loopo_driver_node.py
+++ loopo_driver/loopo_driver/loopo_driver_node.py
@@ -2,18 +2,11 @@
 
 from loopo_driver.loopo_driver import LoopODriver
 
-from loopo_messages.msg import ExtensionActuatorUpdate, LoopActuatorUpdate, TwistActuatorUpdate, LoopOCommand
-from loopo_messages.srv import SendLoopOCommand
+from loopo_messages.srv import SendCommand
 
 import rclpy
 from rclpy.node import Node
-from rclpy.executors import MultiThreadedExecutor
-from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 
-#gripper = LoopODriver()
-
-parallel_group = ReentrantCallbackGroup()
-#mcu_operation_cb_group = MutuallyExclusiveCallbackGroup()
 
 class LoopODriverNode(Node):
     def __init__(self):
@@ -21,16 +14,7 @@ class LoopODriverNode(Node):
 
         self.gripper = LoopODriver()
         
-        self.extension_actuator_publisher = self.create_publisher(ExtensionActuatorUpdate, 'loopo/extension_actuator/status', 10)
-        self.twist_actuator_publisher = self.create_publisher(TwistActuatorUpdate, 'loopo/twist_actuator/status', 10)
-        self.loop_actuator_publisher = self.create_publisher(LoopActuatorUpdate, 'loopo/loop_actuator/status', 10)
-
-        self.update_timer = self.create_timer(0.01, self.gripper_update, parallel_group)
-        self.publisher_timer = self.create_timer(0.1, self.publisher_callback, parallel_group)
-        
-        self.rate = self.create_rate(0.005)
-
-        self.command_service = self.create_service(SendLoopOCommand, 'loopo_command', self.send_command, callback_group=parallel_group)
+        self.command_service = self.create_service(SendCommand, 'loopo_command', self.send_command)
 
     def gripper_update(self):
         self.gripper.update()
@@ -38,50 +22,29 @@ class LoopODriverNode(Node):
 
     def send_command(self, request, response):
         self.gripper.send_command(request.id, request.command, request.value)
-        while (self.gripper.ack == 0):
-            self.gripper_update()
-            self.get_logger().info('ACK: %d' % (self.gripper.ack))
-            self.rate.sleep()
-        response.sent = True
+
+        response.ex_position = self.gripper.ex_position
+        response.ex_status =   self.gripper.ex_status
+        response.ex_control =  self.gripper.ex_control
+
+        response.tw_size =     self.gripper.tw_size
+        response.tw_offset =   self.gripper.tw_offset
+        response.tw_status =   self.gripper.tw_statuts
+        response.tw_control =  self.gripper.tw_control
+
+        response.lp_size =     self.gripper.lp_size
+        response.lp_force =    self.gripper.force
+        response.lp_status =   self.gripper.lp_status
+        response.lp_control =  self.gripper.lp_control
+        
         return response
-
-    def publisher_callback(self):
-        extension_msg = ExtensionActuatorUpdate()
-        loop_msg = LoopActuatorUpdate()
-        twist_msg = TwistActuatorUpdate()
-
-        extension_msg.position = self.gripper.ex_position
-        extension_msg.status = self.gripper.ex_status
-        extension_msg.control = self.gripper.ex_control
-
-        loop_msg.size = self.gripper.lp_position
-        loop_msg.force = self.gripper.force
-        loop_msg.status = self.gripper.lp_status
-        loop_msg.control = self.gripper.lp_control
-
-        twist_msg.size = self.gripper.tw_position
-        twist_msg.offset = self.gripper.tw_offset
-        twist_msg.status = self.gripper.tw_statuts
-        twist_msg.control = self.gripper.tw_control
-
-        self.extension_actuator_publisher.publish(extension_msg)
-        self.twist_actuator_publisher.publish(twist_msg)
-        self.loop_actuator_publisher.publish(loop_msg)
-
-        return 1
 
 def main(args=None):
     rclpy.init(args=args)
 
-    executor = MultiThreadedExecutor()
-
     loopo_driver_node = LoopODriverNode()
 
-    #rclpy.spin(loopo_driver_node)
-
-    executor.add_node(loopo_driver_node)
-
-    executor.spin()
+    rclpy.spin(loopo_driver_node)
     
     loopo_driver_node.destroy_node()
 
