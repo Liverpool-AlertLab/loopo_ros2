74bb467249f09cde3af09be0c4b970ceccd735e7
lupo manes <lupo961@gmail.com>
2023-07-11 08:26:39 +0100

    Stylistic changes for readability

--- loopo_driver/loopo_driver/loopo_driver.py
+++ loopo_driver/loopo_driver/loopo_driver.py
@@ -1,39 +1,40 @@
 import serial
 import re
-from time import sleep
-from threading import *
+
 
 class LoopODriver:
-    def __init__(self, com_port = '/dev/ttyACM0', baud = 115200):
 
-            self._com_port = com_port
-            self._baud = baud
-            self._serial_interface = serial.Serial(self._com_port, self._baud, timeout=1)
+    def __init__(self, com_port='/dev/ttyACM0', baud=115200):
+        self._com_port = com_port
+        self._baud = baud
+        self._serial_interface = serial.Serial(self._com_port,
+                                               self._baud,
+                                               timeout=1)
 
-            self.ex_position = 0
-            self.ex_status = 0
-            self.ex_control = 0
+        self.ex_position = 0
+        self.ex_status = 0
+        self.ex_control = 0
 
-            self.tw_size = 0
-            self.tw_offset = 0
-            self.tw_statuts = 0
-            self.tw_control = 0
+        self.tw_size = 0
+        self.tw_offset = 0
+        self.tw_statuts = 0
+        self.tw_control = 0
 
-            self.lp_size = 0
-            self.force = 0.53
-            self.lp_status = 0
-            self.lp_control = 0
-                
-    def send_command(self, id = None, command = None , value = None):
+        self.lp_size = 0
+        self.force = 0.53
+        self.lp_status = 0
+        self.lp_control = 0
+
+    def send_command(self, id=None, command=None, value=None):
         command = str(id) + str(command) + str(value) + r'\n'
-        
+
         self._serial_interface.write(command.encode('ascii'))
         self._serial_interface.flush()
-        
+
         line = self._serial_interface.readline().decode('ascii').rstrip()
-        
-        values = re.findall(r'\d+(?:\.\d+)?',line)
-            
+
+        values = re.findall(r'\d+(?:\.\d+)?', line)
+
         self.ex_position = int(values[0])
         self.ex_status = int(values[1])
         self.ex_control = int(values[2])
@@ -47,7 +48,3 @@ class LoopODriver:
         self.force = float(values[8])
         self.lp_status = int(values[9])
         self.lp_control = int(values[10])
-
-
-
-
--- loopo_driver/loopo_driver/loopo_driver_node.py
+++ loopo_driver/loopo_driver/loopo_driver_node.py
@@ -13,8 +13,9 @@ class LoopODriverNode(Node):
         super().__init__('loopo_driver_node')
 
         self.gripper = LoopODriver()
-        
-        self.command_service = self.create_service(SendCommand, 'loopo_command', self.send_command)
+        self.command_service = self.create_service(SendCommand,
+                                                   'loopo_command',
+                                                   self.send_command)
 
     def gripper_update(self):
         self.gripper.update()
@@ -24,31 +25,35 @@ class LoopODriverNode(Node):
         self.gripper.send_command(request.id, request.command, request.value)
 
         response.ex_position = self.gripper.ex_position
-        response.ex_status =   self.gripper.ex_status
-        response.ex_control =  self.gripper.ex_control
+        response.ex_status = self.gripper.ex_status
+        response.ex_control = self.gripper.ex_control
 
-        response.tw_size =     self.gripper.tw_size
-        response.tw_offset =   self.gripper.tw_offset
-        response.tw_status =   self.gripper.tw_statuts
-        response.tw_control =  self.gripper.tw_control
+        response.tw_size + self.gripper.tw_size
+        response.tw_offset = self.gripper.tw_offset
+        response.tw_status = self.gripper.tw_statuts
+        response.tw_control = self.gripper.tw_control
 
-        response.lp_size =     self.gripper.lp_size
-        response.lp_force =    self.gripper.force
-        response.lp_status =   self.gripper.lp_status
-        response.lp_control =  self.gripper.lp_control
-        
+        response.lp_size = self.gripper.lp_size
+        response.lp_force = self.gripper.force
+        response.lp_status = self.gripper.lp_status
+        response.lp_control = self.gripper.lp_control
+
         return response
 
+
 def main(args=None):
+
     rclpy.init(args=args)
 
     loopo_driver_node = LoopODriverNode()
 
     rclpy.spin(loopo_driver_node)
-    
+
     loopo_driver_node.destroy_node()
 
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
+
     main()
