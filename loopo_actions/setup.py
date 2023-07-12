981c3bcbd8facef06c5317395f3a40dbed2c8069
itsameWolf <lupo961@gmail.com>
2023-07-12 17:19:54 +0100

    Added Launch file for action servers

--- loopo_actions/setup.py
+++ loopo_actions/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'loopo_actions'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "extension_actions_server = loopo_actions.extension_actions:main",
+            "twist_actions_server = loopo_actions.twist_actions:main",
+            "loop_actions_server = loopo_actions.twist_actions:main",
         ],
     },
 )
